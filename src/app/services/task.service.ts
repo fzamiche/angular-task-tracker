1a81f60a8609e57069a0e8d73214624ca6102c5a
Fatah ZAMICHE <zamiche.fatah@gmail.com>
2025-01-10 17:52:33 +0100

    Setup http using fake api (json-server) instead of mock

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,17 +1,18 @@
 import { Injectable } from '@angular/core';
-import {TASKS} from "../../assets/mock-tasks";
 import {Task} from "../models/Task";
-import {Observable, of} from "rxjs";
+import {Observable} from "rxjs";
+import {HttpClient} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks';
+
+  constructor(private httpClient : HttpClient) { }
 
   getsTasks(): Observable<Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.httpClient.get<Task[]>(this.apiUrl);
   }
 }
